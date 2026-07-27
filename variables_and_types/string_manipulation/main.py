d516bfd152c9287e235e039d23462edec7e947c4
tobeeze42-beep <>
2026-07-27 20:49:20 +0000

    String manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,6 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+dairy1 = grocery_items[0:4]
+dairy2 = grocery_items[5:11]
+bakery1 = grocery_items[12:17]
+
+print("we have dairy and bakery items: " + dairy1 + " , " + dairy2 + " and " + bakery1 + " in aisla 5 ")
